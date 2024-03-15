bbd158f16c57713be7ed51afeee2753b38b89651
kedevans <nwokedievan7@gmail.com>
2024-03-15 12:55:37 +0100

    fixed changes

--- index.js
+++ index.js
@@ -1,72 +1,27 @@
-// function stickNavbar() {
-//     document.addEventListener("DOMContentLoaded", function(){
-//         window.addEventListener('scroll', function() {
-//             if (window.scrollY > 50) {
-//               document.getElementById('navbar_top').classList.add('fixed-top');
-//               document.getElementById('navbar_top').classList.add('alobam');
-
-//               // add padding top to show content behind navbar
-//             //   navbar_height = document.querySelector('.navbar').offsetHeight;
-//             //   document.body.style.paddingTop = navbar_height + 'px';
-//             } else {
-//               document.getElementById('navbar_top').classList.remove('fixed-top');
-//               document.getElementById('navbar_top').classList.remove('alobam');
-
-//                // remove padding top from body
-//               document.body.style.paddingTop = '0';
-//             } 
-//         });
-//     }); 
-// }
-
-// stickNavbar();
-
-
-// function myFunction(){
-//   document.getElementById('tap').style.display = 'block';
-// } 
-
-
-// document.getElementById('tap').onclick = function(){
-//    myFunction() 
-// };
-
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a').forEach(link =>{
-  if(link.href.includes(`${activePage}`)){
-    link.classList.add('active');
-  }
-})
-
-
-
-
-
-
-
-let smallNav = document.getElementById('navSmall');
-
-let myAim = document.getElementById('myaim');
-
-smallNav.addEventListener('click', function(){
-  // myAim.style.display = 'none'
-
-  if(myAim.style.display === 'none' ){
-
-    myAim.style.display = 'block'
-  }else{
-    myAim.style.display = 'none'
-  }
-})
-
-
-
-
-
-
-
-
-
-
+document.addEventListener("DOMContentLoaded", () => {
+    const menu_toggle = document.getElementById("menu_toggle");
+    menu_toggle.addEventListener("click", () => {
+      const mobile_nav = document.getElementById("sm_ul");
+      mobile_nav.classList.toggle("show_menu");
+    });
   
-
+    // Highlight the current page in the navigation menu
+    const currentPage = window.location.pathname;
+    console.log("Current Page:", currentPage);
+  
+    const navLinks = document.querySelectorAll("nav ul li a");
+  
+    // Default: Highlight the "Home" link
+    const homeLink = document.querySelector("nav ul li a[href='/']");
+    homeLink.classList.add("hideee");
+  
+    navLinks.forEach((link) => {
+      const linkPath = link.getAttribute("href");
+      // Check if the current page exactly matches the link path
+      if (currentPage === linkPath) {
+        link.classList.add("hideee");
+      } else {
+        link.classList.remove("hideee");
+      }
+    });
+  });

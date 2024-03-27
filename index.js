bb1a7d3d0c4a3a27008f2af63ae7a45a86dadea1
kedevans <nwokedievan7@gmail.com>
2024-03-27 10:25:09 +0100

    changes

--- index.js
+++ index.js
@@ -25,3 +25,58 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+
+  
+        function showDetais() {
+            const getDetails = document.querySelector(".Details")
+
+            const myArray = [
+              {
+                id: 0,
+                person_name:"Arole Yusuf Adeshola",
+                heading: "President, Lascohet Alumni",
+                person_img: "/assets/president-2.jpeg",
+                description: ["Former National Treasurer, National Environmental Health Students of Nigeria (NAEHSN) 2003", "Former Vice Chairman, Student Representative Council, Lascohet Yaba 2002/2003","Former Branch Secretary, NULGE Eti-Osa LG 2011/2014", "Former State Publicity Secretary EHOAN Lagos Chapter 2016/2023", "State Secretary EHOAN Lagos Chapter 2023 till date", "Secretary, Epe Grammar School Old Student Association (EGOSA) 1990/1995 set."],
+              }
+            ]
+    
+
+            let data = [];
+
+            myArray.map((item) => {
+                data += `
+                <div class="Details">
+                <h3>${item.person_name}</h3>
+                <span class="president">${item.heading}</span>
+                    <p class="mt-5">
+                      ${myArray.map(description => description.description[0])},
+                      ${myArray.map(description => description.description[1])}
+
+                         Former National Treasurer, National Environmental Health Students of Nigeria (NAEHSN) 2003
+                         Former Vice Chairman, Student Representative Council, Lascohet Yaba 2002/2003
+                    </p>
+
+                    <p>
+                      Former Branch Secretary, NULGE Eti-Osa LG 2011/2014
+                      Former State Publicity Secretary EHOAN Lagos Chapter 2016/2023
+                    </p>
+
+                    <p>
+                      State Secretary EHOAN Lagos Chapter 2023 till date
+                      Secretary, Epe Grammar School Old Student Association (EGOSA) 1990/1995 set.
+                    </p>
+                    <div><img src="/assets/president-2.jpeg" alt=""></div>
+                    </div>
+                `
+            })
+            getDetails.innerHTML = data;
+
+        }
+        
+
+
+
+
+
+

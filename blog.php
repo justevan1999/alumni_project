f458ceef913ffefe9a522340f07c6ff9d759111d
kedevans <nwokedievan7@gmail.com>
2024-09-24 00:12:55 +0100

    changes

--- blog.php
+++ blog.php
@@ -30,13 +30,12 @@
                 class="logo_lg"
               />
               </a>
-             <a href="index.php">
-             <img
+             
+              <img
                 src="assets/logo_rounded.png"
                 alt="Company Brand"
                 class="logo_sm"
               />
-             </a>
             </div>
             <ul>
               <li><a href="index.php">Home</a></li>
@@ -223,7 +222,11 @@
 </div>
   
     
-
+    <script>
+        document.getElementById('home-img').onclick = function(){
+          window.location.href = 'index.php';
+        }
+    </script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script> 
 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
